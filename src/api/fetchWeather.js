917bb3b14b1e84b01e19bc2b323d654f699ac03d
Gregor <nichtgregor@gmail.com>
2021-04-26 17:12:19 +0200

    http to https

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://api.openweathermap.org/data/2.5/weather';
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'bc15c53862c73c45064e4e6c1b32368e';
 
 export const fetchWeather = async (query) => {
